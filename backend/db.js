2a168b1783fc6025e4663b4188cf7b682ae6aec6
Indy61532 <134271924+Indy61532@users.noreply.github.com>
2025-05-30 00:12:40 +0200

    dsdfdsfsf

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://akkodis_user:aiXWtWxkQScREal7TJlhH1h3bMp7jwO2@dpg-d0sd1tili9vc73csjf60-a.oregon-postgres.render.com/akkodis_db',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
