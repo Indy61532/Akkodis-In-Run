9369162d3c566d7da1c6e500ae613472f29897c3
Indy61532 <134271924+Indy61532@users.noreply.github.com>
2025-05-30 00:36:47 +0200

    ffhfhfghhgf

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'akkodis_run',
-  password: 'admin',  // <- tady zadej heslo, které jsi nastavil při instalaci PostgreSQL
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
